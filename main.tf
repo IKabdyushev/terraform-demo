c6dc9f7b32675cd9524839ff62383c6c58c090b3
Ilyas K <36274299+IKabdyushev@users.noreply.github.com>
2021-11-15 20:38:57 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -71,6 +71,16 @@ resource "aws_lb" "mylb" {
   enable_deletion_protection = false
 }
 
+resource "aws_lb_listener" "front_end" {
+  load_balancer_arn = aws_lb.front_end.arn
+  port              = "80"
+  protocol          = "HTTP"
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.myec2.arn
+  }
+}
+
 resource "aws_lb_target_group" "myec2" {
   name        = "testpython"
   port        = 80
