565fd2c70d27fcbced8a97cdb6d5cc22e50f58ef
Ilyas K <36274299+IKabdyushev@users.noreply.github.com>
2021-11-15 20:39:55 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -72,7 +72,7 @@ resource "aws_lb" "mylb" {
 }
 
 resource "aws_lb_listener" "front_end" {
-  load_balancer_arn = aws_lb.front_end.arn
+  load_balancer_arn = aws_lb.mylb.arn
   port              = "80"
   protocol          = "HTTP"
   default_action {
